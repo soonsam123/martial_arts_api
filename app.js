60beac36df2c3a867d7e0a5866feaec31da0dfa4
karatesoon@gmail.com <karatesoon@gmail.com>
2020-12-06 14:42:10 -0300

    Part 2 of our tutorial

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const express = require("express");
 const app = express();
 
 app.listen(8000);
+
+app.get("/dojos", function (req, res) {
+  res.send({
+    id: 1,
+    name: "KARATÊ DOJO",
+    teacher: "JACKSON TANAKA",
+  });
+});
