6d9c5a49095ad4dfe9279babe885b924162e031c
karatesoon@gmail.com <karatesoon@gmail.com>
2020-12-06 15:14:36 -0300

    Part 4 - Configuring headers

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.listen(8000);
 
+/**
+ * Adding headers to our requests.
+ */
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // TODO: Pesquisar se precisa tirar em produção
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.get("/dojos", function (req, res) {
   res.send({
     id: 1,
