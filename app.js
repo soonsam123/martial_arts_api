5124a95b03fc8715376f809ad93d8a4babfaf7da
karatesoon@gmail.com <karatesoon@gmail.com>
2020-12-06 15:15:25 -0300

    Remove comment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(8000);
  * Adding headers to our requests.
  */
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // TODO: Pesquisar se precisa tirar em produção
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
